a9ffc6837fc21fee9025994ad2d817fafa334331
hijiri <hijiri@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-26 02:29:31 +0000

    initial release
    git-svn-id: http://plugins.svn.wordpress.org/dynamic-latest-post-in-nav-menu/trunk@882101 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ dynamic-latest-post-in-nav-menu.php
@@ -0,0 +1,173 @@
+<?php
+/*
+Plugin Name: Dynamic Latest Post in Nav Menu
+Plugin URI: http://hijiriworld.com/web/
+Description: Add Custom Post Type's Dynamic Latest Post and Archive to Nav Menu.
+Author: hijiri
+Author URI: http://hijiriworld.com/web/
+Version: 1.0.0
+*/
+
+/*  Copyright 2014 hijiri
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if( !class_exists( 'Dlpinm' ) )
+{
+	class Dlpinm
+	{
+		function __construct()
+		{
+			load_plugin_textdomain( 'dlpinm', false, basename( dirname( __FILE__ ) ) . '/lang' );
+			add_action( 'admin_head-nav-menus.php', array( $this, 'dlpinm_metabox' ) );
+			add_filter( 'wp_setup_nav_menu_item', array( $this, 'dlpinm_setup_nav_menu_item' ) );
+			add_filter( 'wp_nav_menu_objects', array( $this, 'dlpinm_filter' ), 0 );
+		}
+		
+		function dlpinm_filter( $items )
+		{
+			foreach( $items as $key => $item ) {
+				
+				if ( $item->post_type == 'nav_menu_item' ) {
+					if ( $item->object == 'latest' ) {
+						$recent_custom_post = get_posts( 'numberposts=1&post_type='.$item->type );
+						$latest_url = get_permalink( $recent_custom_post[0]->ID );
+						$item->url = $latest_url;
+						
+						// archive and all single
+						if ( /* get_permalink() == $latest_url ||  */get_query_var( 'post_type' ) == $item->type ) {
+							$item->classes[] = 'current-menu-item';
+							$item->current = true;
+						}
+					} else if ( $item->object == 'archive' ) {
+						$item->url = get_post_type_archive_link( $item->type );
+						
+						// archive and all single
+						if( get_query_var( 'post_type' ) == $item->type ) {
+							$item->classes[] = 'current-menu-item';
+							$item->current = true;
+						}
+					}
+				}
+			}
+			return $items;
+		}
+		
+		function dlpinm_metabox()
+		{
+			add_meta_box( 'dlpinm_archive', __( 'Archive', 'dlpinm' ), array( $this, 'dlpinm_metabox_content_archive' ), 'nav-menus', 'side', 'default' );
+			add_meta_box( 'dlpinm_latest', __( 'Latest', 'dlpinm' ), array( $this, 'dlpinm_metabox_content_latest' ), 'nav-menus', 'side', 'default' );
+		}
+		function dlpinm_metabox_content_archive()
+		{
+			$post_types = get_post_types( array( 'show_in_nav_menus' => true, 'has_archive' => true ), 'object' );
+			
+			unset( $post_types['page']); // exclude Page object
+
+			if( $post_types ) {
+				foreach ( $post_types as $post_type ) {
+					$post_type->classes = array();
+					$post_type->type = $post_type->name;
+					$post_type->object_id = $post_type->name;
+					$post_type->title = $post_type->labels->name;
+					$post_type->object = 'archive';
+					
+					$post_type->menu_item_parent = null;
+					$post_type->url = null;
+					$post_type->target = null;
+					$post_type->attr_title = null;
+					$post_type->xfn = null;
+					$post_type->db_id = null;
+					$post_type->description = null;
+				}
+				$walker = new Walker_Nav_Menu_Checklist( array() );
+?>
+				<div id="dlpinm-archive" class="posttypediv">
+					<div id="tabs-panel-dlpinm" class="tabs-panel tabs-panel-active">
+						<ul id="dlpinm-checklist" class="categorychecklist form-no-clear">
+							<?php //echo walk_nav_menu_tree( array_map('wp_setup_nav_menu_item', $post_types), 0, (object) array( 'walker' => $walker) ); ?>
+							<?php echo walk_nav_menu_tree( array_map('wp_setup_nav_menu_item', $post_types ), 0, (object) array( 'walker' => $walker) ); ?>
+							<?php //echo walk_nav_menu_tree( $post_types, 0, (object) array( 'walker' => $walker) ); ?>
+						</ul>
+					</div>
+				</div>
+				<p class="button-controls">
+					<span class="add-to-menu">
+						<!-- <img class="waiting" src="<?php echo esc_url( admin_url( 'images/wpspin_light.gif' ) ); ?>" alt="" /> -->
+						<input type="submit"<?php //disabled( $nav_menu_selected_id, 0 ); ?> class="button-secondary submit-add-to-menu" value="<?php esc_attr_e( 'Add to Menu' ); ?>" name="add-dlpinm-menu-item" id="submit-dlpinm-archive" />
+					</span>
+				</p>
+<?php
+			}
+		}
+		function dlpinm_metabox_content_latest()
+		{
+			$post_types = get_post_types( array( 'show_in_nav_menus' => true ), 'object' );
+			
+			unset( $post_types['page']); // exclude Page object
+			
+			if( $post_types ) {
+				foreach ( $post_types as $post_type ) {
+					$post_type->classes = array();
+					$post_type->type = $post_type->name;
+					$post_type->object_id = $post_type->name;
+					$post_type->title = $post_type->labels->name;
+					$post_type->object = 'latest';
+					
+					$post_type->menu_item_parent = null;
+					$post_type->url = null;
+					$post_type->target = null;
+					$post_type->attr_title = null;
+					$post_type->xfn = null;
+					$post_type->db_id = null;
+					$post_type->description = null;
+				}
+				$walker = new Walker_Nav_Menu_Checklist( array() );
+?>
+				<div id="dlpinm-latest" class="posttypediv">
+					<div id="tabs-panel-dlpinm" class="tabs-panel tabs-panel-active">
+						<ul id="dlpinm-checklist" class="categorychecklist form-no-clear">
+							<?php echo walk_nav_menu_tree( array_map('wp_setup_nav_menu_item', $post_types ), 0, (object) array( 'walker' => $walker) ); ?>
+						</ul>
+					</div>
+				</div>
+				<p class="button-controls">
+					<span class="add-to-menu">
+						<!-- <img class="waiting" src="<?php echo esc_url( admin_url( 'images/wpspin_light.gif' ) ); ?>" alt="" /> -->
+						<input type="submit"<?php //disabled( $nav_menu_selected_id, 0 ); ?> class="button-secondary submit-add-to-menu" value="<?php esc_attr_e( 'Add to Menu' ); ?>" name="add-dlpinm-menu-item" id="submit-dlpinm-latest" />
+					</span>
+				</p>
+<?php
+			}
+		}
+		
+		function dlpinm_setup_nav_menu_item( $item )
+		{
+			if ( isset( $item->object ) ) {
+				if ( $item->object == 'latest' ) {
+					$item->type_label = __( 'Latest', 'dlpinm');
+	
+				} else if ( $item->object == 'archive' ) {
+					$item->type_label = __( 'Archive', 'dlpinm');
+				}
+			}
+			return $item;
+		}
+	}
+	
+	$Dlpinm = new Dlpinm();
+}
+
+?>
